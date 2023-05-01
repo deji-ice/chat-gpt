88c3d66afc15b8634e3cece06313b0941d13acb5
Atanda Ayodeji <ayodejiatanda1@gmail.com>
2023-05-01 18:41:31 +0100

    chat component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -5,6 +5,7 @@ import { useSession } from "next-auth/react";
 import React from "react";
 import { useCollection } from "react-firebase-hooks/firestore";
 import Message from "./Message";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
 
 type Props = {
   chatId: string;
@@ -23,15 +24,25 @@ const Chat = ({ chatId }: Props) => {
           chatId,
           "messages"
         ),
-        orderBy("createdAt", "asc") 
+        orderBy("createdAt", "asc")
       )
   );
 
-  return <div className="flex-1">
-    {messages?.docs.map((message)=>(
-      <Message key={message.id} message={message.data()}/>
-    ))}
-  </div>;
+  return (
+    <div className="flex-1 overflow-y-auto overflow-x-hidden">
+      {messages?.empty && (
+        <>
+          <p className="text-center mt-10 text-white">
+            Type a prompt below to test out ChatGPT 😊{" "}
+          </p>
+          <ArrowDownCircleIcon className="animate-bounce w-10 h-10 mt-5 mx-auto text-white" />
+        </>
+      )}
+      {messages?.docs.map((message) => (
+        <Message key={message.id} message={message.data()} />
+      ))}
+    </div>
+  );
 };
 
 export default Chat;
